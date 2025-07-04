d49197bb258d26579f18f207889b72e0fb56902a
Todokage <ftdraco176@gmail.com>
2025-07-04 11:22:14 +0300

    deploy1

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
+
   email: { type: String, unique: true },
   password: String,
   isAdmin: { type: Boolean, default: false },
